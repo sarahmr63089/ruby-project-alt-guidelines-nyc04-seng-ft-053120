fc5d08ca9c229e8c835600d0515132d83bddb030
sarahmr63089 <sarahrose63089@gmail.com>
2020-07-08 10:33:07 -0400

    mostly clean but one extra menu method

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,16 +1,44 @@
 class CLI  
-  # this method gets or creates the user-company
+  # this method gets or creates the user-company -- revise get company to just get company name, separate method for u&p validation, separate method for create a new company
   def get_company
     puts "Please enter your company name:"
     company_name = gets.strip
 
     company = Company.find_by(name: company_name)
     if company == nil
-      company = Company.create({ :name => company_name })
+      puts "Please provide a username and password."
+      puts "Username:"
+      username = gets.strip
+      puts "Password:"
+      password = gets.strip
+      company = Company.create({ :name => company_name, :password => password, :username => username })
+    end
+
+    if get_username_and_test_validity(company)
+      if get_password_and_test_validity(company)
+      else 
+        puts "That is not the correct password, please try again."
+        return
+      end
+    else 
+      puts "That is not the correct username, please try again."
+      return
     end
     company
   end
-  
+
+  def get_username_and_test_validity(company)
+    prompt = TTY::Prompt.new
+    username = prompt.ask("Please enter your username:")
+    company.username == username
+  end
+
+  def get_password_and_test_validity(company)
+    prompt = TTY::Prompt.new
+    password = prompt.mask("Please enter your password:")
+    company.password == password
+  end
+
   # this method finds and displays the franchises owned by the user-company
   def display_franchises(company)
     puts "#{company.name} owns these franchises:"
@@ -18,87 +46,64 @@ class CLI
   end
 
   # this method checks if the user-company owns the franchise with the given id
+  # this method was rejecting existing franchises in testing, reconfigure to boolean
   def valid_franchise_choice(company, id)
     if !company.franchises.exists?(id)
       puts "Please select an id from the list above next time or find an owner to purchase a franchise!" 
     end
   end
 
-# require 'tty-prompt'
-# require 'active_record'
-# require_relative '../config/environment'
+  # this method changes the owner of an existing franchise that belongs to the user-company
+  def change_franchise_owner
+    company = get_company
+    display_franchises(company)
+    puts "Please enter the id number for the franchise whose ownership has changed."
+    id = gets.strip
+    # is this a valid choice? -- reconfigure this method
+    if !valid_franchise_choice(company, id)
+      return 
+    else 
+      puts "Please enter the name of the new owner."
+      new_owner = gets.strip
+      if !Owner.exists?(name: new_owner)
+        Owner.create(name: new_owner)
+      end
 
-# class CLI
-#   # this method gets or creates the user-company
-#   def get_company
-#     puts "Please enter your company name:"
-#     company_name = gets.strip
-#     # if company doesn't exist create new company -- snag from Jacie's create method
-#     company = Company.find_by(name: company_name)
-#   end
-  
-#   # this method finds and displays the franchises owned by the user-company
-#   def display_franchises(company)
-#     puts "#{company.name} owns these franchises:"
-#     company.franchises.select { |franchise| puts "Franchise #{franchise.id} in #{franchise.location}" }
-#   end
+      franchise = Franchise.where(id: id).update(owner_id: Owner.find_by(name: new_owner).id)
+      franchise.select { |franchise| puts "Franchise #{franchise.id} has new owner #{franchise.owner.name}." }
+    end
+  end
 
-#   # this method checks if the user-company owns the franchise with the given id
-#   def valid_franchise_choice(company, id)
-#     # to be a valid choice, the franchise must be one owned by the company
-#     if !company.franchises.exists?(id)
-#       puts "Please select an id from the list above next time or find an owner to purchase a franchise!" 
-#     end
-#   end
-  
-#   # this method changes the owner of an existing franchise that belongs to the user-company
-#   def change_franchise_owner(company)
-#     display_franchises(company)
-#     puts "Please enter the id number for the franchise whose ownership has changed."
-#     id = gets.strip
-#     # is this a valid choice? -- method needs finishing
-#     if !valid_franchise_choice(company, id)
-#       return 
-#     else 
-#       puts "Please enter the name of the new owner."
-#       new_owner = gets.strip
-#       if !Owner.exists?(name: new_owner)
-#         Owner.create(name: new_owner)
-#       end
+  # this method closes an franchise that belongs to the user-company
+  def delete_franchise
+    company = get_company
+    display_franchises(company)
+    puts "Please enter the id number for the franchise you would like to close."
+    id = gets.strip
+    # valid choice?
+    valid_franchise_choice(company, id)
 
-#       franchise = Franchise.where(id: id).update(owner_id: Owner.find_by(name: new_owner).id)
-#       franchise.select { |franchise| puts "Franchise #{franchise.id} has new owner #{franchise.owner.name}." }
-#     end
-#   end
+    Franchise.find(id).destroy
+    puts "Franchise #{id} has been closed."
+  end
 
-#   # this method closes an franchise that belongs to the user-company
-#   def delete_franchise(company)
-#     display_franchises(company)
-#     puts "Please enter the id number for the franchise you would like to close."
-#     id = gets.strip
-#     # valid choice?
-#     valid_franchise_choice(company, id)
-
-#     Franchise.find(id).destroy
-#     puts "Franchise #{id} has been closed."
-#   end
-  
-
-  # this method shows the highest-earning franchise
+  # this method shows the highest-earning franchise -- reconfigure to show highest earning franchise for the company-user
   def franchise_with_highest_profit
     big_earner = Franchise.all.max_by { |franchise| franchise.profit }
     puts "Franchise #{big_earner.id} has the highest profit, having earned $#{big_earner.profit}. It is owned by #{big_earner.owner.name} with parent company #{big_earner.company.name}."
   end
 
   #create a new franchise
-  def create_franchise(company)
+  def create_franchise
+    company = get_company
+
     puts "Please enter Owner name:"
     owner_name = gets.chomp
     owner = Owner.find_by name: owner_name
     if owner == nil
       owner = Owner.create({ :name => owner_name })
     end
-    # ask for location
+    #ask for location
     puts "What is the location?"
     location = gets.chomp
 
@@ -130,45 +135,27 @@ class CLI
     franchise = Franchise.find_by location: franchise_location
     puts "This location's profit is $#{franchise.profit}."
   end
+
+  def search(prompt)
+    lookup_options = [
+      {"By Owner"=> -> do owner_and_franchises end},
+      {"By Location"=> -> do location end }, 
+      {"Franchise with highest profit"=> -> do franchise_with_highest_profit end}, 
+      {"Exit" => -> do exit end}
+    ]
+    prompt.select('What would you like to search for?', lookup_options)
+  end
   
-  def get_company
-    puts "Please enter your company name:"
-    company_name = gets.strip
-
-    company = Company.find_by(name: company_name)
-    if company == nil
-      puts "Please provide a username and password."
-      puts "Username:"
-      username = gets.strip
-      puts "Password:"
-      password = gets.strip
-      company = Company.create({ :name => company_name, :password => password, :username => username })
-    end
-
-    if get_username_and_test_validity(company)
-      if get_password_and_test_validity(company)
-      else 
-        puts "That is not the correct password, please try again."
-        return
-      end
-    else 
-      puts "That is not the correct username, please try again."
-      return
-    end
-
-    company
-  end
-
-  def get_username_and_test_validity(company)
+  def menu
     prompt = TTY::Prompt.new
-    username = prompt.ask("Please enter your username:")
-    company.username == username
-  end
-
-  def get_password_and_test_validity(company)
-    prompt = TTY::Prompt.new
-    password = prompt.mask("Please enter your password:")
-    company.password == password
+    menu_options = [
+      {"Create Franchise"=> -> do create_franchise end},
+      {"Delete Franchise"=> -> do delete_franchise end }, 
+      {"Change Owner of Franchise"=> -> do change_franchise_owner end}, 
+      {"Search"=> -> do search(prompt) end},
+      {"Exit" => -> do exit end}
+    ]
+    prompt.select('What do you want to do?', menu_options)
   end
 
   def run
@@ -179,71 +166,6 @@ class CLI
     # menu
   end
 end
-#   # this method shows the highest-earning franchise
-#   def franchise_with_highest_profit
-#     big_earner = Franchise.all.max_by { |franchise| franchise.profit }
-#     puts "Franchise #{big_earner.id} has the highest profit, having earned #{big_earner.profit}. It is owned by #{big_earner.owner.name} with parent company #{big_earner.company.name}."
-#   end
-# end
-# #create a new franchise
-# def create_franchise
-#   puts "Please enter Company name:"
-#   company_name = gets.chomp
-#   company = Company.find_by name: company_name
-#   if company == nil
-#       company = Company.create({ :name => company_name })
-#   end
-
-#   puts "Please enter Owner name:"
-#   owner_name = gets.chomp
-#   owner = Owner.find_by name: owner_name
-#   if owner == nil
-#       owner = Owner.create({ :name => owner_name })
-#   end
-# #ask for location
-#   puts "What is the location?"
-#   location = gets.chomp
-
-#   Franchise.create({ 
-#       :profit => 0, 
-#       :location => location, 
-#       :owner_id => owner.id,
-#       :company_id => company.id
-#   })
-
-#   puts "Complete!"
-# end
-
-
-# def owner_and_franchises
-#   puts "Please enter owner name to see franchises:"
-#   owner_name = gets.chomp
-#   owner = Owner.find_by name: owner_name
-#   franchise = Franchise.find_by owner_id: owner.id
-#   company = Company.find_by id: franchise.company_id
-  
-#   puts "This owner owns franchise #{franchise.id} in #{franchise.location},
-#    with parent company #{company.name}."
-# end
-
-# #enter location to see profit
-# def location
-#   puts "Please enter location to see profit:"
-#   franchise_location = gets.chomp
-#   franchise = Franchise.find_by location: franchise_location
-#   puts "This location's profit is #{franchise.profit}"
-# end
-
-
-# puts "Welcome to the Franchise Infomatic!"
-
-# ActiveRecord::Base.logger = nil
-
-# cli = CLI.new
-
-# #company = cli.get_company
-
-# #cli.change_franchise_owner(company)
 
 
 # def menu
@@ -265,5 +187,3 @@ end
 #     # lookup_options = ["By Owner", "By Location,","Franchise with highest profit"]
 #     # prompt.select('What would you like to search for?', lookup_option)
 #   end
-
-#   menu 
